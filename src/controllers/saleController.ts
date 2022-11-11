90eae3400595b76181101839b598262a4a7d98e1
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-11-10 21:25:55 -0300

    paginação completa

--- src/controllers/saleController.ts
+++ src/controllers/saleController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { Sale } from '../models/Sale';
 import { Store } from '../models/Store';
 import { Modality } from '../models/Modality';
-import { info } from 'console';
+import { Op } from 'sequelize';
 
 export const newSale = async (req: Request, res: Response) => {
     
@@ -24,7 +24,50 @@ export const newSale = async (req: Request, res: Response) => {
 
 export const removeSale = async (req: Request, res: Response) => {
     let id: number = parseInt(req.params.id);
-    await Sale.destroy({ where: { id } })
-    res.status(200)
+    await Sale.destroy({ where: { id } });
+    res.status(200);
     return res.json({});
+}
+
+export const listSale = async (req: Request, res: Response) => {
+    const {dataInicio, dataFim} = req.query;
+    
+    // incluindo os models Modality e Store para realizar inner joins e receber o nome da modalidade e loja
+    const list = await Sale.findAll({
+            include: [{
+                model: Modality,
+                required: true,
+     
+             }, {
+                 model: Store,
+                 required: true
+             }],
+            where: { 
+                createdAt: { [Op.between]: [`${dataInicio} 00:00:01.000 +00:00`, `${dataFim} 23:59:59.000 +00:00`]}}
+            });
+    
+    res.status(200);
+    return res.json({list});
+}
+
+export const paginationSale = async (req: Request, res: Response) => {
+    const page: number = parseInt(req.params.page);
+    const npp: number = 2;
+    const offset: number = (page - 1 ) * npp;
+    // incluindo os models Modality e Store para realizar inner joins e receber o nome da modalidade e loja
+    const list = await Sale.findAll({
+            include: [{
+                model: Modality,
+                required: true,
+     
+             }, {
+                 model: Store,
+                 required: true
+             }],
+             offset,
+             limit: npp
+            });
+    
+    res.status(200);
+    return res.json({list});
 }
--- src/routes/saleRoutes.ts
+++ src/routes/saleRoutes.ts
@@ -6,6 +6,8 @@ const router = Router();
 
 router.post('/sale', SaleController.newSale);
 router.delete('/sale/:id', SaleController.removeSale);
+router.get('/sales/list', SaleController.listSale);
+router.get('/sales/page/:page', SaleController.paginationSale);
 
 
 export default router;
